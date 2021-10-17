0de80a47d4b4f9c6cea8497819389aba2275700f
SubhahsreeSatapathy <satapathysubhashree2000@gmail.com>
2021-10-17 10:10:53 +0530

    add components

---
+++ src/components/Masthead.js
@@ -0,0 +1,34 @@
+import React from "react";
+import avataaars from "../img/avataaars.svg";
+
+const Masthead=()=>{
+    return (
+      <header className="masthead bg-primary text-white text-center">
+        <div className="container d-flex align-items-center flex-column">
+          {/* <img
+            className="masthead-avatar mb-5"
+            src="../../public/img/avataaars.svg"
+            alt="..."
+          /> */}
+          <img src={avataaars} alt="the boy" className="masthead-avatar mb-5" />
+          <h1 className="masthead-heading text-uppercase mb-0">
+            Start Bootstrap
+          </h1>
+
+          <div className="divider-custom divider-light">
+            <div className="divider-custom-line"></div>
+            <div className="divider-custom-icon">
+              <i className="fas fa-star"></i>
+            </div>
+            <div className="divider-custom-line"></div>
+          </div>
+
+          <p className="masthead-subheading font-weight-light mb-0">
+            Graphic Artist - Web Designer - Illustrator
+          </p>
+        </div>
+      </header>
+    );
+
+}
+export default Masthead;
